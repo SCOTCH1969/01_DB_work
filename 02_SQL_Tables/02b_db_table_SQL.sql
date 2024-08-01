08c9c4f0a4a1aa63ec101f29c0156a62e99ceb45
SCOTCH <hubert.baier@freenet.de>
2024-08-01 08:56:03 +0200

    01

--- 02_SQL_Tables/02b_db_table_SQL.sql
+++ 02_SQL_Tables/02b_db_table_SQL.sql
@@ -15,17 +15,28 @@ CREATE DATABASE IF NOT EXISTS boo;
 USE boo;
 
 /* Tabelle test löschen, falls vorhanden*/
-
+DROP TABLE IF EXISTS test;
 
 /* Tab. test anlegen, falls noch nicht vorhanden*/
+CREATE TABLE test
+(
+	name VARCHAR(20),
+	age INT
+);
 
 
 /* Alle Tabellen in der DB anzeigen */
-#SHOW TABLES;
+SHOW TABLES;
 
 /* Struktur der Tabelle anzeigen */
+DESCRIBE test;
 
 /* ----- Daten ------- */
+INSERT INTO test(name,age) VALUES ("Grizabella",29);
+INSERT INTO test(age,name) VALUES (35,"Alonzo");
+INSERT INTO test VALUES ();
+
+SELECT * FROM test;
 
 
 -- ABER: Doppelte Datensätze werden zugelassen !
